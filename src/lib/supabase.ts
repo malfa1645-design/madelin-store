9cd0d1c98e904e350d6a41f4ec20a9cacc353ccc
malfa1645-design <malfa1645@gmail.com>
2026-06-21 22:05:35 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 // 1. اذهب إلى supabase.com وأنشئ مشروعاً مجانياً
 // 2. من الإعدادات (API)، انسخ الـ URL والـ Anon Key
 const supabaseUrl = 'https://ciuczjgcseokjafxaudv.supabase.co';
-const supabaseKey = 'https://ciuczjgcseokjafxaudv.supabase.co/rest/v1/';
+const supabaseKey = 'sb_publishable_9BY82vRIgaaMnpS1_tc-Dw__doZ6RbV';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
